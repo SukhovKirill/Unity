ee5cccdfc5ef97d92d51465abf15b6ac1d7e3f42
SukhovKirill <160316848+SukhovKirill@users.noreply.github.com>
2024-05-05 15:53:24 +0300

    Add files via upload

---
+++ pr4_1.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour
+{
+    [SerializeField]
+    private int myInt;
+
+    [SerializeField]
+    private float myFloat;
+
+    [SerializeField]
+    private string myString;
+}
